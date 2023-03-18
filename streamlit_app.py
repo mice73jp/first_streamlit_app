45e0a0470ab2e10d50318d72f0927bbf8671b689
Aozora <mice73jp@users.noreply.github.com>
2023-03-18 21:47:55 +0900

    Update streamlit_app.py
    Added try except statements for handling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,21 +27,19 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json()) # just write the data to the screen 
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-
+execpt URLError as e:
+  streamlit.error()
+    
 streamlit.stop()  # avoiding to run below code frequently
-# import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
